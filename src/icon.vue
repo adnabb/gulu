aa6cda70a5e11e0ce2d0efdf8a49f98f21109a60
chenshiyu <chenshiyu@ygsoft.com>
2020-01-08 15:31:13 +0800

    feat:添加组件icon.vue

---
+++ src/icon.vue
@@ -0,0 +1,15 @@
+<template>
+    <svg class="icon-gulu" aria-hidden="true">
+        <use xlink:href="#g-download">
+    </svg>
+</template>
+
+<script>
+    export default {
+        
+    }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
