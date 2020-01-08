daab154f0bda636eebbbce233ba6fb76db0c35f8
chenshiyu <chenshiyu@ygsoft.com>
2020-01-08 14:59:51 +0800

    feat:引入parcel打包vue文件

---
+++ src/button.vue
@@ -0,0 +1,31 @@
+<template>
+  <button class="g-button">按钮</button>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style lang="scss" scoped>
+.g-button {
+  background-color: var(--button-bg);
+  border-radius: var(--border-radius);
+  border: 1px solid var(--border-color);
+  height: var(--button-height);
+  line-height: 1;
+  padding: 0 1em;
+  cursor: pointer;
+
+  &:active {
+    border-color: var(--borer-active-color);
+    color: var(--font-active-color);
+  }
+
+  &:focus {
+    background-color: var(--button-active-bg);
+    color: var(--font-hover-color);
+    border-color: var(--border-hover-color);
+    outline: none;
+  }
+}
+</style>
