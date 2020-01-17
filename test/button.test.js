743e708dfce60fa7f0eb1a2c19661ed774b1d8b9
chenshiyu <chenshiyu@ygsoft.com>
2020-01-17 10:05:45 +0800

    feat:button新增slot传值的测试用例
    feat:重新整理button的测试用例格式

--- test/button.test.js
+++ test/button.test.js
@@ -1,4 +1,5 @@
 import Vue from 'vue';
+import { shallowMount } from '@vue/test-utils';
 import Button from '../src/button';
 
 Vue.config.productionTip = false;
@@ -11,82 +12,99 @@ describe('Button', () => {
     expect(Button).to.exist;
   });
 
-  it('可以设置Icon', () => {
-    const vm = getRenderedVM(Button, {
-      icon: 'download'
+  describe('props', () => {
+    it('可以slot传值', () => {
+      const wrapper = shallowMount(Button, {
+        slots: {
+          default: '按钮',
+        },
+      });
+
+      const button = wrapper.vm.$el.querySelector('.button-text');
+      const buttonText = button.innerText;
+
+      expect(buttonText).to.eq('按钮');
     });
 
-    const use = vm.$el.querySelector('use');
-    const href = use.getAttribute('xlink:href');
+    it('可以设置Icon', () => {
+      const vm = getRenderedVM(Button, {
+        icon: 'download'
+      });
 
-    expect(href).to.equal('#g-download');
+      const use = vm.$el.querySelector('use');
+      const href = use.getAttribute('xlink:href');
 
-    removeTestVM(vm);
-  });
+      expect(href).to.equal('#g-download');
 
-  it('可以设置loading', () => {
-    const vm = getRenderedVM(Button, {
-      icon: 'download',
-      loading: true
+      removeTestVM(vm);
     });
 
-    const use = vm.$el.querySelector('use');
-    const href = use.getAttribute('xlink:href');
+    it('可以设置loading', () => {
+      const vm = getRenderedVM(Button, {
+        icon: 'download',
+        loading: true
+      });
 
-    expect(href).to.equal('#g-loading');
+      const use = vm.$el.querySelector('use');
+      const href = use.getAttribute('xlink:href');
 
-    removeTestVM(vm);
+      expect(href).to.equal('#g-loading');
+
+      removeTestVM(vm);
+    });
+
+    it('icon默认为order为1', () => {
+      const div = appendTestDom();
+
+      const vm = getRenderedVM(Button, {
+        icon: 'download',
+        iconPosition: 'left'
+      }, div);
+
+      const svg = vm.$el.querySelector('svg');
+      const { order } = window.getComputedStyle(svg);
+
+      expect(order).to.equal('1');
+
+      removeTestVM(vm);
+    });
+
+    it('icon可以根据iconPosition改变order', () => {
+      const div = appendTestDom();
+
+      const vm = getRenderedVM(Button, {
+        icon: 'download',
+        iconPosition: 'right'
+      }, div);
+
+      const svg = vm.$el.querySelector('svg');
+      const { order } = window.getComputedStyle(svg);
+
+      expect(order).to.equal('2');
+
+      removeTestVM(vm);
+    });
   });
 
-  it('icon默认为order为1', () => {
-    const div = appendTestDom();
+  describe('events', () => {
+    it('可以点击button触发click事件', () => {
+      const div = appendTestDom();
 
-    const vm = getRenderedVM(Button, {
-      icon: 'download',
-      iconPosition: 'left'
-    }, div);
+      const vm = getRenderedVM(Button, {
+        icon: 'download',
+      }, div);
 
-    const svg = vm.$el.querySelector('svg');
-    const { order } = window.getComputedStyle(svg);
+      const callback = sinon.fake();
 
-    expect(order).to.equal('1');
+      vm.$on('click', callback);
 
-    removeTestVM(vm);
+      vm.$el.click();
+
+      expect(callback).to.have.been.called;
+
+      removeTestVM(vm);
+    });
   });
-
-  it('icon可以根据iconPosition改变order', () => {
-    const div = appendTestDom();
-
-    const vm = getRenderedVM(Button, {
-      icon: 'download',
-      iconPosition: 'right'
-    }, div);
-  
-    const svg = vm.$el.querySelector('svg');
-    const { order } = window.getComputedStyle(svg);
-  
-    expect(order).to.equal('2');
-  
-    removeTestVM(vm);
-  });
-
-  it('可以点击button触发click事件', () => {
-    const div = appendTestDom();
-
-    const vm = getRenderedVM(Button, {
-      icon: 'download',
-    }, div);
-  
-    const callback = sinon.fake();
-  
-    vm.$on('click', callback);
-  
-    vm.$el.click();
-  
-    expect(callback).to.have.been.called;
-  
-    removeTestVM(vm);
-  })
 });
 
 function getRenderedVM(component, propsData, mountDom) {
